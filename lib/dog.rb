1eac1e0b5f150d3769da030a1964f7fb4969ed85
Sarah Harrs <sarahharrs@gmail.com>
2019-07-16 21:54:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
-# Add your code here
+class Dog
+  attr_accessor :name
+  @@all = []
+
+  def initalize(name)
+    @name=name
+    @@all<<self
+  end
+
+  def self.all
+    @@all
+  end
+
+def self.print_all
+print_all={}
+  @@all.each do |all|
+    if print_all[all]
+      puts " #{name}"
+end
+end
+end
+
+
+  def self.clear_all
+    @@all.clear
+  end
+
+end
